b7c48d692ff8f5529301198c53152035bcee8904
jeremyfrank33 <frank81@purdue.edu>
2021-10-26 19:48:59 -0400

    added notes for Nick

--- updatesql.py
+++ updatesql.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 import pymysql
 
+# https://docs.rackspace.com/support/how-to/install-mysql-server-on-the-ubuntu-operating-system/
+
 engine = create_engine('mysql+pymysql://root:hello123@localhost/kenpom')
 all_years.to_sql(name = 'teams', con = engine, if_exists = 'replace')
