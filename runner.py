b273704031f412849c571458e957bf493cddd7b8
Joe Zobel <zobelj@purdue.edu>
2021-10-26 19:33:54 -0400

    Improved printing of randomized teams

--- runner.py
+++ runner.py
@@ -1,8 +1,16 @@
 from randomize import getRandTeams
 from updatecsv import update_csv
 
+def printTeam(side, team):
+    print(f"\n{side}:")
+    for user in team.keys():
+        print(f"{user} : {team[user][0]}")
+
 ringers, ballerz = getRandTeams()
-ringersScore = int(input("Ringers score: "))
+printTeam("Ringers", ringers)
+printTeam("Ballerz", ballerz)
+
+ringersScore = int(input("\nRingers score: "))
 ballerzScore = int(input("Ballerz score: "))
 
 update_csv("./NBA 2K Blacktop.csv", ringers, ballerz, ringersScore, ballerzScore)
