0ef0c905aff71f0c7b6008ccadeea61590aab441
jeremyfrank33 <frank81@purdue.edu>
2021-10-27 01:30:22 -0400

    add PF and PA pg to get_user_records

--- sql_scripts/get_user_records.sql
+++ sql_scripts/get_user_records.sql
@@ -1,5 +1,5 @@
 #User records
-select UserID, sum(PF > PA) as W, sum(PF < PA) as L from user_gamelogs
+select UserID, sum(PF > PA) as W, sum(PF < PA) as L, round(avg(PF),1) as PF, round(avg(PA),1) as PA from user_gamelogs
 group by UserID
 order by W desc;
 
