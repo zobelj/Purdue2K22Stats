64506e761e4d3df6b6c4d50e25e7e353fc8d5a4d
jeremyfrank33 <frank81@purdue.edu>
2021-10-26 23:36:02 -0400

    update sql files

--- sql_scripts/get_player_records.sql
+++ sql_scripts/get_player_records.sql
@@ -1,3 +1,4 @@
+
 select Name, sum(PF > PA) as W, sum(PF < PA) as L from user_gamelogs
 join players using(PlayerID)
 group by PlayerID
---
+++ sql_scripts/get_user_records.sql
@@ -0,0 +1,14 @@
+#User records
+select UserID, sum(PF > PA) as W, sum(PF < PA) as L from user_gamelogs
+group by UserID
+order by W desc;
+
+
+#User combinations
+select concat(u1.UserID," and ", u2.UserID) as Names, sum(u1.PF > u1.PA) as W, sum(u1.PF < u1.PA) as L from user_gamelogs u1
+join user_gamelogs u2 on u1.GameID = u2.GameID and u1.TeamID = u2.TeamID and u1.UserID != u2.UserID
+where u1.UserID < u2.UserID
+group by u1.UserID, u2.UserID
+order by W desc;
+
+
