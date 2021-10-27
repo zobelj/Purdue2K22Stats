6a7faa1454091f7f9ba631463f6e822fbd4f94c3
jeremyfrank33 <frank81@purdue.edu>
2021-10-27 00:27:31 -0400

    add reg wins and losses to get_player_records

--- sql_scripts/get_player_records.sql
+++ sql_scripts/get_player_records.sql
@@ -1,5 +1,5 @@
-select Name, sum(PF > PA) as W, sum(PF < PA) as L, round(sum(PF > PA) / count(Name), 3) as 'wpct' from user_gamelogs
+select Name, sum(PF > PA) as W, sum(PF < PA) as L, round(sum(PF > PA) / count(Name), 3) as 'wpct', 
+sum((PF = 21 and PF > PA) or (PF = 22 and PA <= 19)) as regW, sum((PA = 21 and PA > PF) or (PA = 22 and PF <= 19)) as regL from user_gamelogs
 join players using(PlayerID)
 group by PlayerID
 order by W desc;
-
