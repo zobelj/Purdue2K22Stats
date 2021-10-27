d5ace567f6c6bcbe457cfe6b186aaebdfe242373
Joe Zobel <zobelj@purdue.edu>
2021-10-26 23:35:24 -0400

    Sending link to Jeremy

--- updatesql.py
+++ updatesql.py
@@ -5,3 +5,5 @@ def update_user_gamelogs(csv_name):
     data = pd.read_csv(csv_name)
     engine = create_engine('mysql+pymysql://sql5447048:muZwPef3av@sql5.freemysqlhosting.net/sql5447048')
     data.to_sql(name = 'user_gamelogs', con = engine, if_exists = 'replace')
+
+# https://www.plus2net.com/python/mysql-insert-sqlalchemy.php
