8fe05ebf214bbf2ebe2ba687b18790523961fdc0
jeremyfrank33 <frank81@purdue.edu>
2021-10-27 00:20:16 -0400

    round wpct to 3 decimals

--- sql_scripts/get_player_records.sql
+++ sql_scripts/get_player_records.sql
@@ -1,4 +1,4 @@
-select Name, sum(PF > PA) as W, sum(PF < PA) as L, round(sum(PF > PA) / count(Name), 2) as 'Win Percentage' from user_gamelogs
+select Name, sum(PF > PA) as W, sum(PF < PA) as L, round(sum(PF > PA) / count(Name), 3) as 'wpct' from user_gamelogs
 join players using(PlayerID)
 group by PlayerID
 order by W desc;
