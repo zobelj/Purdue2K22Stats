1d60f7f0e3aa6d4b0d78e72ff69a7865c3dc0511
jeremyfrank33 <frank81@purdue.edu>
2021-11-02 18:39:26 -0400

    modified player user records sql

--- sql_scripts/player_user_records.sql
+++ sql_scripts/player_user_records.sql
@@ -1,4 +1,4 @@
 select UserID, Name, sum(PF > PA) as W, sum(PA > PF) as L, sum(PF > PA)/count(*) as wpct from user_gamelogs 
 join players using(PlayerID)
 group by UserID, Name
-order by W desc;
+order by W desc, wpct desc;
